0f07b49f80fdb0994a597bb3870fcac008f47c0a
Ajmal Fayiz <fayizjmal47@gmail.com>
2026-07-20 16:20:27 +0530

    Get request Updated

--- index.js
+++ index.js
@@ -2,10 +2,38 @@ const express = require('express')
 
 const app = express()
 
+let users = [
+    {
+        "id": 1,
+        "Name": "Ajmal",
+        "Age": 25
+    },
+    {
+        "id": 2,
+        "Name": "Midlaj",
+        "Age": 20
+    },
+    {
+        "id": 3,
+        "Name": "Fayiz",
+        "Age": 26
+    }
+]
 const PORT = 4000;
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
+    res.send("Home Page")
+})
+app.get('/about', (req, res) => {
+    res.send("About Page")
+})
+
+app.get('/contact', (req, res) => {
+    res.send("Contact Page")
+})
+
+app.get('/users', (req, res) => {
+    res.json(users)
 })
 
 app.listen(PORT, () => {
