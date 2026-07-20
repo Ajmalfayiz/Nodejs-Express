f6bdfd326eeee83842afaf46361f77aac4a6ed3d
Ajmal Fayiz <fayizjmal47@gmail.com>
2026-07-20 17:11:20 +0530

    post Request Updated

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const express = require('express')
+const express = require('express');
+const { message } = require('statuses');
 
 const app = express()
 
 const PORT = 4000;
+
+//Middle ware
+app.use(express.json())
 
 let users = [
     {
@@ -33,8 +37,8 @@ app.get('/users', (req, res) => {
 app.get('/users/:id', (req, res) => {
     const user_id = Number(req.params.id)
     const user = users.find((user) => user.id === user_id)
- 
-   // console.log(user,'user')
+
+    // console.log(user,'user')
 
     if (!user) {
         return res.status(404).json({
@@ -44,7 +48,32 @@ app.get('/users/:id', (req, res) => {
     res.json(user)
 })
 
+//Create User
 
+app.post('/users', (req, res) => {
+
+    // console.log(req.body)
+
+    const { name, age } = req.body
+
+    if (!name || !age) {
+        return res.status(400).json({
+            message: "Name and Age are Required"
+        })
+    }
+
+    const newUser = {
+        id: users.length + 1,
+        name,
+        age
+    }
+
+    users.push(newUser)
+
+    res.status(201).json({
+        message: 'User Created Successfully'
+    })
+})
 app.listen(PORT, () => {
     console.log(`Server Running  on ${PORT}`)
 })
