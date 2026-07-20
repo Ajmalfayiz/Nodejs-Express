6c3e52c092d4078b4ab976640d6a2daa16e124a7
Ajmal Fayiz <fayizjmal47@gmail.com>
2026-07-20 16:46:56 +0530

    get users by id

--- index.js
+++ index.js
@@ -22,14 +22,28 @@ let users = [
     }
 ]
 
+//Get all users
 
 app.get('/users', (req, res) => {
     res.json(users)
 })
 
+//Get user by id
+
 app.get('/users/:id', (req, res) => {
-    res.json()
+    const user_id = Number(req.params.id)
+    const user = users.find((user) => user.id === user_id)
+ 
+   // console.log(user,'user')
+
+    if (!user) {
+        return res.status(404).json({
+            message: "User Not Found"
+        })
+    }
+    res.json(user)
 })
+
 
 app.listen(PORT, () => {
     console.log(`Server Running  on ${PORT}`)
