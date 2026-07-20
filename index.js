e94e057f4339c4f2a45a3b9b1516dcf96ba32e73
Ajmal Fayiz <fayizjmal47@gmail.com>
2026-07-20 16:32:47 +0530

    express-server branch Updated

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ app.get('/', (req, res) => {
     res.send("Hello World")
 })
 
+app.get('/', (req, res) => {
+    res.send("Home Page")
+})
+app.get('/about', (req, res) => {
+    res.send("About Page")
+})
+
+app.get('/contact', (req, res) => {
+    res.send("Contact Page")
+})
+
 app.listen(PORT, () => {
     console.log(`Server Running  on ${PORT}`)
 })
